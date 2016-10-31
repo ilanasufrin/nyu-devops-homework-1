97b789c24a40efb9fb7382df0607a2518e5c5cbd
skyline220 <nm2564@nyu.edu>
2016-10-31 16:30:02 +0000

    Updated resource state method, still isn't working properly.

--- icecream.py
+++ icecream.py
@@ -153,20 +153,19 @@ def list_resources_by_type():
 
 @app.route('/ice-creams/', methods=['PUT'])
 def  put_ice_cream_status():
-     payload = json.loads(request.data)
      statusupdate = request.args.get('status')
      if statusupdate == 'melt':
           for key, value in icecreams.iteritems():
-               status = icecreams[key]['status']
-               if status == 'frozen':
-                    icecreams[icecreams[key][0]['id']] = {'name': payload['name'], 'description': payload['description'], 'status' : 'melted', 'base': payload['base'], 'price':payload['price'], 'popularity': payload['popularity']}
-                    rc = HTTP_200_OK
+#              status = icecreams[key]['status']
+#              if status == 'frozen':
+               icecreams[key]['status'] = 'melted'
+               rc = HTTP_200_OK
      if statusupdate == 'freeze':
           for key, value in icecreams.iteritems():
-               status = icecreams[key]['status']
-               if status == 'melt':
-                    icecreams[icecreams[key][0]['id']]  = {'name': payload['name'], 'description': payload['description'], 'status' : 'frozen', 'base': payload['base'], 'price':payload['price'], 'popularity': payload['popularity']}
-                    rc = HTTP_200_OK
+#              status = icecreams[key]['status']
+#              if status == 'melt':
+               icecreams[key]['status'] = 'frozen'
+               rc = HTTP_200_OK
      else:
           message = { 'error' : 'No ice creams were found therefore none could have there status changed to'  %  statusupdate}
           rc = HTTP_404_NOT_FOUND
